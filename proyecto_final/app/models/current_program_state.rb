e89740174df212fc38a6859bbea5c56b3b47b0af
Matías <matias.bustos@redcomercio.cl>
2022-11-21 17:04:23 -0300

    more validations!

--- proyecto_final/app/models/current_program_state.rb
+++ proyecto_final/app/models/current_program_state.rb
@@ -1,5 +1,8 @@
 class CurrentProgramState < ApplicationRecord
-  validate :screen_range
+  validate :screen_range, 
+           :check_current_velocity_range, 
+           :check_max_velocity_range, 
+           :velocity_mode_range
   validates :screen, presence: true
 
 
@@ -7,6 +10,30 @@ class CurrentProgramState < ApplicationRecord
     if screen.present?
       if !(screen >= 1 && screen <= 8)
         errors.add(:screen_range, "El numero de la pantalla debe estar en un rango del 1 al 8")
+      end
+    end
+  end
+
+  def velocity_mode_range
+    if velocity_mode.present?
+      if !(velocity_mode >= 0 && velocity_mode <= 2)
+        errors.add(:velocity_mode_range, "El modo de velocidad debe de estar entre el 0 y el 2 inclusive")
+      end
+    end
+  end
+
+  def check_current_velocity_range
+    if max_velocity.present?
+      if !(max_velocity >= 0 && max_velocity <= 180)
+        errors.add(:max_velocity, "El rango de la actual velocidad (current_velocity) debe de estar entre el 0 y el 180 inclusive")
+      end
+    end
+  end
+
+  def check_max_velocity_range
+    if max_velocity.present?
+      if !(max_velocity >= 0 && max_velocity <= 180)
+        errors.add(:max_velocity, "El rango de velocidad maxima (max_velocity) debe de estar entre el 0 y el 180 inclusive")
       end
     end
   end
--- proyecto_final/app/models/task.rb
+++ proyecto_final/app/models/task.rb
@@ -1,7 +1,19 @@
 class Task < ApplicationRecord
-  validate :velocity_mode_range, :check_max_velocity_range, :check_current_velocity_range
+  validate :velocity_mode_range, 
+           :check_max_velocity_range, 
+           :check_current_velocity_range, 
+           :screen_range
 
   validates :screen, presence: true
+
+
+  def screen_range
+    if screen.present?
+      if !(screen >= 1 && screen <= 8)
+        errors.add(:screen_range, "El numero de la pantalla debe estar en un rango del 1 al 8")
+      end
+    end
+  end
 
 
   def velocity_mode_range
@@ -15,7 +27,7 @@ class Task < ApplicationRecord
   def check_current_velocity_range
     if max_velocity.present?
       if !(max_velocity >= 0 && max_velocity <= 180)
-        errors.add(:max_velocity, "El rango de velocidad maxima debe de estar entre el 0 y el 180 inclusive")
+        errors.add(:max_velocity, "El rango de la actual velocidad (current_velocity) debe de estar entre el 0 y el 180 inclusive")
       end
     end
   end
@@ -23,7 +35,7 @@ class Task < ApplicationRecord
   def check_max_velocity_range
     if max_velocity.present?
       if !(max_velocity >= 0 && max_velocity <= 180)
-        errors.add(:max_velocity, "El rango de velocidad maxima debe de estar entre el 0 y el 180 inclusive")
+        errors.add(:max_velocity, "El rango de velocidad maxima (max_velocity) debe de estar entre el 0 y el 180 inclusive")
       end
     end
   end
